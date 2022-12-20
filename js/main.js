f74b784ba57065e25f0bce3e3163291f0b2ac859
techmemo <119870287+techmemo@users.noreply.github.com>
2022-12-20 11:01:54 -0500

    BIG JAVASCRIPT STUFF
    hope this works!!!!!!!

--- js/main.js
+++ js/main.js
@@ -17,18 +17,6 @@ function getFolder() {
   return loc[loc.length - 2];
 }
 
-// every article box text div has id "big-[htmlfilename]"
-function articleBox(title, desc, art, date) {
-  document.getElementById("big-" + art).innerHTML =
-    "<h3>" +
-    title +
-    "</h3><h5>by Matthew Nudelman - " +
-    date +
-    "</h5><br /><p>" +
-    desc +
-    "</p>";
-}
-
 function setMonth(f) {
   switch (f) {
     case "1":
@@ -86,6 +74,99 @@ const dates2 = [
   { month: "December", day: 3 },
   { month: "December", day: 10 },
 ];
+
+// every article box text div has id "big-[htmlfilename]"
+function articleBox(title, desc, art, date) {
+  document.getElementById("big-" + art).innerHTML =
+    "<h3>" +
+    title +
+    "</h3><h5>by Matthew Nudelman - " +
+    date +
+    "</h5><br /><p>" +
+    desc +
+    "</p>";
+}
+
+const big = [
+  // for article lists
+  {
+    id: "ios16",
+    title: "iOS 16 - Major Features and Future Updates",
+    desc: "This fall, users of Apple devices can expect several updates to iOS including features both new and previously-announced.",
+    month: "November",
+    day: "30",
+    year: "2022",
+    alt: "An icon with the number 16 representing iOS 16, the newest version of iOS.",
+    hover:
+      "An icon with the number 16 representing iOS 16, the newest version of iOS.",
+  },
+  {
+    id: "ipad10",
+    title: "iPad 10 - The End of the Lightning Port",
+    desc: "Apple recently announced a redesigned 10th-generation iPad, making the 9th-generation iPad the last to include built-in Lightning connectivity. However, the connector is not yet gone from the iPad...",
+    month: "December",
+    day: "3",
+    year: "2022",
+    alt: "The 10th-generation iPad.",
+    hover: "The 10th-generation iPad.",
+  },
+  {
+    id: "pixelwatch",
+    title: "Google Pixel Watch",
+    desc: "Google recently entered the smartwatch game, leaving many to wonder how they intend to compete with Apple and Samsung on wearable technology.",
+    month: "December",
+    day: "10",
+    year: "2022",
+    alt: "The Google Pixel Watch.",
+    hover: "The first Google Pixel Watch.",
+  },
+];
+
+function newArtBox(n) {
+  document.getElementById("big-" + big[n].id).innerHTML =
+    '<div class="big-article"><div class="row"><div class="col-lg-2 col-sm-3"><div class="big-frame"><img class="new-img" src="/img/' +
+    id +
+    '.png" alt="' +
+    big[n].alt +
+    '" title="' +
+    big[n].hover +
+    '"/></div></div><div class="col-sm-10"><h3> ' +
+    big[n].title +
+    "</h3><h5> " +
+    big[n].month +
+    " " +
+    big[n].day +
+    ", " +
+    big[n].year +
+    "</h5><br /><p>" +
+    big[n].desc +
+    "</p></div></div></div>";
+}
+
+/*
+              '<div class="big-article">
+                <div class="row">
+                  <div class="col-lg-2 col-sm-3">
+                    <div class="big-frame">
+                      <img
+                        class="new-img"
+                        src="/img/' + id +'.png"
+                        alt="' + big[n].alt + 
+                        "'title="' + big[n].hover +
+                      "'/>
+                    </div>
+                  </div>
+                  <div class="col-sm-10">
+                    <h3> '+ big[n].title + '</h3>
+                    <h5> '+ big[n].month + ' ' + big[n] + ', ' + big[n].year + '</h5>
+                    <br />
+                    <p>'
+                      + big[n].desc + 
+                    '</p>
+                  </div>
+                </div>
+              </div>'
+*/
 
 /* 
 
