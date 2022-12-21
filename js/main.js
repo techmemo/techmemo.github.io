22911c74eb6b831425393808c26d4fd1981175d3
techmemo <119870287+techmemo@users.noreply.github.com>
2022-12-21 09:56:25 -0500

    automatic article list
    javascript (awesome)

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ const dates2 = [
 ];
 
 // every article box text div has id "big-[htmlfilename]"
-function articleBox(title, desc, art, date) {
+function oldArticleBox(title, desc, art, date) {
   document.getElementById("big-" + art).innerHTML =
     "<h3>" +
     title +
@@ -87,13 +87,71 @@ function articleBox(title, desc, art, date) {
     "</p>";
 }
 
+function articleBox(n) {
+  let bigLink = document.createElement("a");
+  bigLink.href = "/" + big[n].year + "/" + big[n].m + "/" + big[n].id + ".html";
+  document.getElementById("big-list").appendChild(bigLink);
+
+  let bigArticle = document.createElement("div");
+  bigArticle.classList = "big-article";
+  bigLink.appendChild(bigArticle);
+
+  let row = document.createElement("div");
+  row.classList = "row";
+  bigArticle.appendChild(row);
+
+  let cola = document.createElement("div");
+  cola.classList = "col-lg-2 col-sm-3";
+  row.appendChild(cola);
+
+  let imgFrame = document.createElement("div");
+  imgFrame.classList = "big-frame";
+  cola.appendChild(imgFrame);
+
+  let image = document.createElement("img");
+  image.src = "/img/" + big[n].id + ".png";
+  image.classList = "new-img";
+  image.alt = big[n].alt;
+  image.title = big[n].hover;
+  imgFrame.appendChild(image);
+
+  let colb = document.createElement("div");
+  colb.classList = "col-sm-10";
+  row.appendChild(colb);
+
+  let bigTitle = document.createElement("h3");
+  bigTitle.innerText = big[n].title;
+  colb.appendChild(bigTitle);
+
+  const bigDate = document.createElement("h5");
+  bigDate.innerText =
+    "by " +
+    big[n].author +
+    " - " +
+    big[n].month +
+    " " +
+    big[n].day +
+    ", " +
+    big[n].year;
+  colb.appendChild(bigDate);
+
+  const lineBreak = document.createElement("br");
+  colb.appendChild(lineBreak);
+
+  const bigDesc = document.createElement("p");
+  bigDesc.innerText = big[n].desc;
+  colb.appendChild(bigDesc);
+}
+
 const big = [
   // for article lists
   {
     id: "ios16",
+    author: "Matthew Nudelman",
     title: "iOS 16 - Major Features and Future Updates",
     desc: "This fall, users of Apple devices can expect several updates to iOS including features both new and previously-announced.",
     month: "November",
+    m: "11",
     day: "30",
     year: "2022",
     alt: "An icon with the number 16 representing iOS 16, the newest version of iOS.",
@@ -102,9 +160,11 @@ const big = [
   },
   {
     id: "ipad10",
+    author: "Matthew Nudelman",
     title: "iPad 10 - The End of the Lightning Port",
     desc: "Apple recently announced a redesigned 10th-generation iPad, making the 9th-generation iPad the last to include built-in Lightning connectivity. However, the connector is not yet gone from the iPad...",
     month: "December",
+    m: "12",
     day: "3",
     year: "2022",
     alt: "The 10th-generation iPad.",
@@ -112,9 +172,11 @@ const big = [
   },
   {
     id: "pixelwatch",
+    author: "Matthew Nudelman",
     title: "Google Pixel Watch",
     desc: "Google recently entered the smartwatch game, leaving many to wonder how they intend to compete with Apple and Samsung on wearable technology.",
     month: "December",
+    m: "12",
     day: "10",
     year: "2022",
     alt: "The Google Pixel Watch.",
@@ -122,7 +184,7 @@ const big = [
   },
 ];
 
-function newArtBox(n) {
+/* function newArtB(n) {
   document.getElementById("big-" + big[n].id).innerHTML =
     "<div class='big-article'><div class='row'><div class='col-lg-2 col-sm-3'><div class='big-frame'><img class='new-img' src='/img/" +
     big[n].id +
@@ -130,9 +192,9 @@ function newArtBox(n) {
     big[n].alt +
     "' title='" +
     big[n].hover +
-    "'/></div></div><div class='col-sm-10'><h3> " +
+    "'/></div></div><div class='col-sm-10'><h3>" +
     big[n].title +
-    "</h3><h5> " +
+    "</h3><h5>" +
     big[n].month +
     " " +
     big[n].day +
@@ -143,6 +205,25 @@ function newArtBox(n) {
     "</p></div></div></div>";
 }
 
+/* 
+"<div class='big-article'><div class='row'><div class='col-lg-2 col-sm-3'><div class='big-frame'><img class='new-img' src='/img/" +
+  big[n].id +
+  ".png' alt='" +
+  big[n].alt +
+  "' title='" +
+  big[n].hover +
+  "'/></div></div><div class='col-sm-10'><h3>" +
+  big[n].title +
+  "</h3><h5>" +
+  big[n].month +
+  " " +
+  big[n].day +
+  ", " +
+  big[n].year +
+  "</h5><br /><p>" +
+  big[n].desc +
+  "</p></div></div></div>";
+*/
 /*
               '<div class="big-article">
                 <div class="row">
